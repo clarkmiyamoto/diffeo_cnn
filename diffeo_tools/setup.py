60d72eb15ba5ae985ec5f1d87e02a9dc3c6216e1
clarkmiyamoto <cmiyamot@usc.edu>
2024-08-07 23:59:51 -0400

    added diffeo_tools

---
+++ diffeo_tools/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='diffeo_tools',
+    version='0',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
